ffd227159c08f42876444341bad78a0ab841ae97
Lew Coggins <lewis.coggins@noaa.gov>
2023-10-17 17:19:50 -0700

    Added episodic mortality functionality and can now loop over simulations (slow)
    I also added parameter and controls to the appropriate files

--- MetaPVA.R
+++ MetaPVA.R
@@ -47,18 +47,21 @@ ep.spr <- controls$ep.spr
 recfail <- controls$recfail
 ep.fec <- controls$ep.fec
 opt <- controls$opt
+extir.threshold<-controls$extir.threshold
 
-R0 <- parameters$R0           # unfished equilibrium recruitment
-reck <- parameters$reck       # recruitment compensation ratio
-K <- parameters$K             # von Bertalanffy metabolic parameter
-Madult <- parameters$Madult   # vector of age-specific survival (after age at recruitment) [length=A-AR+1]
-lh <- parameters$lh           # length at 50% capture probability (logistic function)
-afec <- parameters$afec       # slope of fecundity-weight relationship
-Wmat <- parameters$Wmat       # weight at maturity
-Ms <- parameters$Ms           # maximum survival by stanza
-Bs <- parameters$Bs           # stanza-specific density effect (can be interpretted as amount of available habitat)
-No <- parameters$No           # initial vulnerable abundance (used to create initial population)
-sd.S <- parameters$sd.S       # standard deviation of environmental effect on survival
+R0 <- parameters$R0                   # unfished equilibrium recruitment
+reck <- parameters$reck               # recruitment compensation ratio
+K <- parameters$K                     # von Bertalanffy metabolic parameter
+Madult <- parameters$Madult           # vector of age-specific survival (after age at recruitment) [length=A-AR+1]
+lh <- parameters$lh                   # length at 50% capture probability (logistic function)
+afec <- parameters$afec               # slope of fecundity-weight relationship
+Wmat <- parameters$Wmat               # weight at maturity
+Ms <- parameters$Ms                   # maximum survival by stanza
+Bs <- parameters$Bs                   # stanza-specific density effect (can be interpretted as amount of available habitat)
+No <- parameters$No                   # initial vulnerable abundance (used to create initial population)
+sd.S <- parameters$sd.S               # standard deviation of environmental effect on survival
+n.pops<-parameters$n.pops             # number of populations 
+Rinit.Scale<-parameters$Rinit.Scale   #relative change in initial recruitment between population 1 and other populations
 
 
 age <- AR:A                                     # ages
@@ -71,27 +74,36 @@ lx <- c(1,Sa[1:(A-AR)])                         # incomplete survivorship to age
 lx <- cumprod(lx)                               # survivorship
 phie <- sum(lx*fec)                             # unfished eggs per recruit
 R.A <- reck/phie                                # alpha of recruitment function 
-R.B <- (reck-1)/(R0*phie)                       # beta of Beverton-Holt recruitment function
-n.pops<-5
+R.B <- (reck-1)/(R0*phie)                       # beta of Beverton-Holt recruitment function for each population
 
 
-Rinit=rep(NA,n.pops)              # initial recruitment given initial population
-Rinit[1]=No/sum(lx)
-ScalePop2to5=c(1.5,2.2,2.4,2.6)
-Rinit[2:5]=Rinit[1]*ScalePop2to5
-PopN1=rep(NA,n.pops)
-PopN1=colSums(sapply(Rinit,'*',lx))             # initial abundance
 
-moveProbs=matrix(c(.3,.1,.1,.25,.25,
-                   .0,.6,.1,.2,.1,
-                   .6 ,.1 ,.1,.1,.1,
-                   .1 ,.1 ,.1,.4,.3,
-                   .1 ,.1 ,.1,.2,.5),5,5,byrow=T)
 
-#moveProbs=diag(5)
+Rinit<-rep(NA,n.pops)              # initial recruitment given initial population size
+Rinit[1]<-No/sum(lx)
+ScalePop2to5<-c(1.5,2.2,2.4,2.6)   #first Crack at scaling each population
+Rinit[2:5]<-Rinit[1]*ScalePop2to5
+PopN1<-rep(NA,n.pops)
+PopN1<-colSums(sapply(Rinit,'*',lx))             # initial abundance
 
+moveProbs<-matrix(c(.3 ,.1 ,.1,.25,.25,
+                    .0 ,.6 ,.1,.2, .1,
+                    .6 ,.1 ,.1,.1, .1,
+                    .1 ,.1 ,.1,.4, .3,
+                    .1 ,.1 ,.1,.2, .5),5,5,byrow=T)
+
+#moveProbs<-diag(5)
+
+
+
+Abun.Results=array(NA,c(nT,n.pops,n.sim))
+Extir.Results=matrix(NA,n.sim,n.pops)
+
+for(j in 1:n.sim){
+#print(j)
+start <- Sys.time()
 rec.dev <- matrix(exp(rnorm((A-AR+1)*n.pops,0,sd.S)+0.5*sd.S^2),nrow=A-AR+1,ncol=n.pops)  # annual recruitment to initialize each population
-anom <- matrix(exp(rnorm((nT-1)*n.pops,0,sd.S)),nT-1,n.pops)
+anom <-    matrix(exp(rnorm((nT-1)*n.pops,0,sd.S)),nT-1,n.pops)
 
 
 #Initialize Populations
@@ -104,31 +116,66 @@ for (i in 1:n.pops){
   
 }
 
+#Episodic Definition
+ep=0
+if(ep.fr <= nT){
+  n.samp <- as.integer(nT/ep.fr)
+  ep <- replicate(1,sample(nT,n.samp,replace=FALSE))         # Years when episodes happen for each population
+  if(n.samp>1){ep.p <- cbind(rep(NA,n.samp),ep[1])}else{ep.p <- cbind(rep(NA,n.samp),ep[1])}
+  if(opt){
+    ep <- rbind(ep,ep.p)
+  }
+  n.ep <- dim(ep.p)[1]
+}
+
 #Initialize Movement Array
-moves=array(NA,c(n.pops,A,n.pops))
+moves<-array(NA,c(n.pops,A,n.pops))
 
 #Compute the rest of the population dynamics
 for(t in 2:nT){
-  
+    #episotic events
+    ep.S <- rep(1,(A-AR+1))
+    svec <- rep(1,(A-AR))
+    if(t%in% ep){
+      svec[(AR+1):amat-AR] <- 1-ep.J.M
+      svec[amat:A-AR] <- 1-ep.A.M
+      #print(svec)
+    }
+    
     for(i in 1:n.pops){
-      Pops[t,AR,i] <- rbinom(1,as.integer(pmax(0,EtPops[t-1,i])),pmin(R.A*anom[t-1,i]/(1+R.B*EtPops[t-1,i]),1))
-      Pops[t,(AR+1):A,i] <- rbinom(A-AR,pmax(0,Pops[t-1,AR:(A-1),i]),Sa[1:(A-AR)])  # survive fish to the next age
+      Pops[t,AR,i] <- rbinom(1,as.integer(pmax(0,EtPops[t-1,i])),pmin(R.A*anom[t-1,i]/(1+R.B[i]*EtPops[t-1,i]),1))
+      Pops[t,(AR+1):A,i] <- rbinom(A-AR,pmax(0,Pops[t-1,AR:(A-1),i]),Sa[1:(A-AR)]*svec)  # survive fish to the next age
       EtPops[t,i] <- as.integer(sum(Pops[t,AR:A,i]*fec))
-      moves[,,i]=sapply(Pops[t,,i],rmultinom,n=1,prob=moveProbs[i,]);sum(moves[,,i])
+      moves[,,i]<-sapply(Pops[t,,i],rmultinom,n=1,prob=moveProbs[i,]);sum(moves[,,i])
     }
 
 #Redistribute the Fish after Movement    
     for(i in 1:n.pops){
-      Pops[t,,i]=rowSums(moves[i,,1:n.pops])
+      Pops[t,,i]<-rowSums(moves[i,,1:n.pops])
     }
 
+    
   }
 
-plot(1:200,rowSums(Pops[,,1]),type="l",ylim=c(0,50000),col=1)
-lines(1:200,rowSums(Pops[,,2]),col=2)
-lines(1:200,rowSums(Pops[,,3]),col=3)
-lines(1:200,rowSums(Pops[,,4]),col=4)
-lines(1:200,rowSums(Pops[,,5]),col=5)
+
+
+Abun.Results[,,j]<-apply(Pops,3,rowSums)
+Extir.Results[j,]<-apply(Abun.Results[1:100,,j],2,min)<=extir.threshold
+runtime <- Sys.time()-start
+print(runtime)    
+
+
+}
+
+print(colSums(Extir.Results)/n.sim)
+
+#plot(1:200,rowSums(Pops[,,1]),type="l",ylim=c(0,75000),col=1)
+#lines(1:200,rowSums(Pops[,,2]),col=2)
+#lines(1:200,rowSums(Pops[,,3]),col=3)
+#lines(1:200,rowSums(Pops[,,4]),col=4)
+#lines(1:200,rowSums(Pops[,,5]),col=5)
+
+matplot((Abun.Results[,1,1:50]),type='l')
 
 
 
