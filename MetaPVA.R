a8bd3fd5938976159cc9d17df03728856a4c64d0
Lew Coggins <lewis.coggins@noaa.gov>
2023-10-22 18:42:46 -0700

    10/22/23 update

--- MetaPVA.R
+++ MetaPVA.R
@@ -95,8 +95,8 @@ for (j in 1:n.pops){
 R.A <- reck/phie                                # alpha of recruitment function 
 R.B <- (reck-1)/(R0*phie)                       # beta of Beverton-Holt recruitment function for each population
 
-#moveProbs<-diag(7)
 
+moveProbs<-diag(n.pops)
 
 
 Abun.Results=array(NA,c(nT,n.pops,n.sim))
@@ -146,10 +146,18 @@ for(t in 2:nT){
     }
     
     for(i in 1:n.pops){
-      Pops[t,AR,i] <- rbinom(1,as.integer(pmax(0,EtPops[t-1,i])),pmin(R.A[i]*anom[t-1,i]/(1+R.B[i]*EtPops[t-1,i]),1))
-      Pops[t,(AR+1):A,i] <- rbinom(A-AR,pmax(0,Pops[t-1,AR:(A-1),i]),Sa[i,1:(A-AR)]*svec)  # survive fish to the next age
-      EtPops[t,i] <- as.integer(sum(Pops[t,AR:A,i]*fec))
-      moves[,,i]<-sapply(Pops[t,,i],rmultinom,n=1,prob=moveProbs[i,]);sum(moves[,,i])
+      #Pops[t,AR,i] <- rbinom(1,as.integer(pmax(0,EtPops[t-1,i])),pmin(R.A[i]*anom[t-1,i]/(1+R.B[i]*EtPops[t-1,i]),1))
+      Pops[t,AR,i] <- as.integer((R.A[i]*EtPops[t-1,i]*anom[t-1,i])/(1+R.B[i]*EtPops[t-1,i]))
+      Pops[t,AR,i] <- as.integer((R.A[i]*EtPops[t-1,i])/(1+R.B[i]*EtPops[t-1,i]))
+      
+      
+      Pops[t,(AR+1):A,i] <- rbinom(A-AR,as.integer(pmax(0,Pops[t-1,AR:(A-1),i])),Sa[i,1:(A-AR)]*svec)  # survive fish to the next age
+      Pops[t,(AR+1):A,i] <- (Pops[t-1,AR:(A-1),i]*Sa[i,1:(A-AR)]*svec)  # survive fish to the next age
+      
+      
+      
+      EtPops[t,i] <- as.integer(sum(Pops[t,AR:A,i]*fec));print(EtPops[t,i])
+      moves[,,i]<-as.integer(sapply(Pops[t,,i],rmultinom,n=1,prob=moveProbs[i,]));sum(moves[,,i])
       #moves[,amat:A-AR,i]<-sapply(Pops[t,amat:A-AR,i],rmultinom,n=1,prob=moveProbs[i,]);sum(moves[,,i])
       
     }
@@ -175,14 +183,14 @@ print(runtime)
 
 print(colSums(Extir.Results)/n.sim)
 
-plot(1:200,rowSums(Pops[,,1]),type="l",ylim=c(0,6000),col=1)
+plot(1:200,rowSums(Pops[,,1]),type="l",ylim=c(0,8000),col=1)
 lines(1:200,rowSums(Pops[,,2]),col=2)
 lines(1:200,rowSums(Pops[,,3]),col=3)
 lines(1:200,rowSums(Pops[,,4]),col=7)
 lines(1:200,rowSums(Pops[,,5]),col=5)
 lines(1:200,rowSums(Pops[,,6]),col=6)
 lines(1:200,rowSums(Pops[,,7]),col=4)
-legend("topright",legend=c("Pearl","Pasc","Esca","Yell","Choc","Apal","Suwa"),lty=1,col=c(1:3,7,5,6,4))
+legend("topleft",legend=c("Pearl","Pasc","Esca","Yell","Choc","Apal","Suwa"),lty=1,col=c(1:3,7,5,6,4),bty='n')
 
 #matplot((Abun.Results[,1,]),type='l')
 
